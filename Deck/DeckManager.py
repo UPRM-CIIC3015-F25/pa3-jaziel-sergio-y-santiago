6e635285021338165ab9307888f9e52ec766b928
jazielrivera <jazie.rivera@upr.edu>
2025-12-06 14:45:10 -0400

    Task 5.1 done

--- Deck/DeckManager.py
+++ Deck/DeckManager.py
@@ -161,6 +161,16 @@ class DeckManager:
         deckJokers = []
 
         priceMap = {
+            "Fibonacci": 5,
+            "Michael Myers": 6,
+            "? Block": 5,
+            "Hogwarts": 6,
+            "StrawHat": 5,
+            "802": 6,
+            "Ogre": 5,
+            "Hog Rider": 4,
+            "Gauntlet": 5,
+            "The Joker": 4,
         }
 
         for name, image in jokerImages.items():
